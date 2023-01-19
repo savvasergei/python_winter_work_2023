e4d640a61d1b3c00e49e1ff2ed359b7422227928
sergei savva <bsospunk@gmail.com>
2023-01-19 22:41:23 +0300

    init

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+x = int(input())
+y = int(input())
+a = (x+y)
+b = (x-y)
+c = (x*y)
+d = (x/y)
+e = (x//y)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if (a>b and a>c and a>d and a>e):
+    print(a)
+elif (b>a and b>c and b>d and b>e):
+    print(b)
+elif (c>a and c>b and c>d and c>e):
+    print(c)
+elif (d>a and d>b and d>c and d>e):
+    print(d)
+elif (e>a and e>b and e>c and e>d):
+    print(e)
+else:
+    print(0)
