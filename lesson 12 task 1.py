bca0e8713cd8722c86b4be6106afcb3eea5a29f8
sergei savva <bsospunk@gmail.com>
2023-02-15 13:22:59 +0300

    lesson 12

---
+++ lesson 12 task 1.py
@@ -0,0 +1,14 @@
+x = list(input())
+res1 = [i for i,j in enumerate(x) if j == min(x)]
+res2 = [i for i,j in enumerate(x) if j == max(x)]
+print(res1)
+print(res2)
+# a = []
+# b = []
+# for i,j in enumerate(x):
+#     if j == min(x):
+#         a.append(i)
+#     if j == max(x):
+#         b.append(i)
+# print(a)
+# print(b)
