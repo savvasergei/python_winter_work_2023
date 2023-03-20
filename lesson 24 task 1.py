92d5958a00a39cfba7195ee610351f900cbc7bf1
sergei savva <bsospunk@gmail.com>
2023-03-20 12:38:38 +0300

    lesson 24

---
+++ lesson 24 task 1.py
@@ -0,0 +1,14 @@
+lst = list(input().split())
+for i in range(len(lst)):
+    for j in range(i + 1, len(lst)):
+        if lst[i] > lst[j]:
+            lst[i], lst[j] = lst[j], lst[i]
+print(lst)
+
+
+
+
+
+
+
+
